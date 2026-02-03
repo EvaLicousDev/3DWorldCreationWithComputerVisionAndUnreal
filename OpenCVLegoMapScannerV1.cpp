f8812c3427032efde5ebfb6ee46a3108e79da097
evali <evali@EVALICIOUSTHING>
2026-02-03 16:07:23 +0000

    Add project files.

---
+++ OpenCVLegoMapScannerV1.cpp
@@ -0,0 +1,12 @@
+﻿// OpenCVLegoMapScannerV1.cpp : Defines the entry point for the application.
+//
+
+#include "OpenCVLegoMapScannerV1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
