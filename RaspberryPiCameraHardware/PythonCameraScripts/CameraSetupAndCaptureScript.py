a117598acd37b74a378839c71cd920e16e3c2f67
Eva Martens <evalicious.dev@outlook.com>
2026-05-06 03:28:33 +0100

    Update CameraSetupAndCaptureScript.py
    System test 1 changes: bug fixes

--- RaspberryPiCameraHardware/PythonCameraScripts/CameraSetupAndCaptureScript.py
+++ RaspberryPiCameraHardware/PythonCameraScripts/CameraSetupAndCaptureScript.py
@@ -1,27 +1,24 @@
 import time
 import cv2
 import numpy as np
-from CameraSetupFunctions import HDR_Img_Camera
+from RPICameraModule import HDR_Img_Camera
 
 from picamera2 import Picamera2
 from pathlib import Path
 from datetime import datetime
 
 ## This script should run when the application request a capture array
-## We weant to send 8 images with various lenghths of exposure
+## We weant to send 8 images with various lengths of exposure
 
 try:
-    #while True:
-    #start the camera
-    # 1) creates preview so system can be set up
-    # 2) creates 4 capture modes based on current light conditions
     camera = HDR_Img_Camera()
     camera.startCam()
     print("Setup complete")
 
-    # capture 8 images, 4 HDR, 4 normal
+    # 4 HDR
     camera.capture_hdr_set()
+    
 except KeyboardInterrupt:
-    print("Finishing up...")
+    print("Input interrupted application")
 finally:
     print("Stopping Camera Application")
